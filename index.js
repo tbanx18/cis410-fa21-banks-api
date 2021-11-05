491fd6a892cc78edb9d44ef270e70d9bb8f23864
tbanx18 <92609076+tbanx18@users.noreply.github.com>
2021-11-05 00:48:10 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const db = require("./dbConnectExec.js");
+
 const app = express();
 
 app.listen(5000, () => {
@@ -12,4 +14,21 @@ app.get("/hi", (req, res) => {
 
 app.get("/", (req, res) => {
   res.send("API is running");
+});
+
+app.get("/Product", (req, res) => {
+  //get data from the database
+  db.executeQuery(
+    `SELECT *
+  FROM Product
+  LEFT JOIN ProductType
+  ON ProductType.ProductTypePK = Product.ProductTypeFK`
+  )
+    .then((theResults) => {
+      res.status(200).send(theResults);
+    })
+    .catch((myError) => {
+      console.log(myError);
+      res.status(500).send();
+    });
 });
